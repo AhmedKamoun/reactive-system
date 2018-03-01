6bddb3b33bb0fc6f7d916a221f8b5cd8572fdeda
Ahmed Kamoun <ahmedkamoun.insat@gmail.com>
2018-03-01 22:34:09 +0100

    code refacto

--- processor/app/com/processors/modules/RegistrationModule.scala
+++ processor/app/com/processors/modules/RegistrationModule.scala
@@ -1,9 +1,5 @@
 package com.processors.modules
 
-import javax.inject.{Inject, Named}
-
-import akka.actor.{ActorRef, ActorSystem}
-import akka.cluster.client.ClusterClientReceptionist
 import com.commons.actors.names.ProcessorsManager
 import com.google.inject.AbstractModule
 import com.processors.actors._
@@ -19,19 +15,7 @@ class RegistrationModule extends AbstractModule with AkkaGuiceSupport {
     bindActorFactory[Processor, Processor.Factory]
     bindActor[ProcessorsManager](ProcessorsManager.NAME)
 
-    // Bind the cluster receptionist actors as an eager singleton
-    bind(classOf[BackendClusterReceptionist]).asEagerSingleton()
   }
 
-
-}
-
-/**
-  * Manages the creation of actors in the backend end.
-  */
-class BackendClusterReceptionist @Inject()(system: ActorSystem,
-                                           @Named(ProcessorsManager.NAME) processorManager: ActorRef) {
-
-  ClusterClientReceptionist(system).registerService(processorManager)
 
 }
